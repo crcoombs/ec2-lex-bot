4fc3d3191ee9c88f1401d86bb5d195058f898515
Cheyne Coombs <cheynecoombs@gmail.com>
2017-06-05 13:04:05 -0400

    Make minor changes based on Pylint

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,6 @@
-
-import boto3
 import os
-import json
-
 from base64 import b64decode
+import boto3
 from botocore.exceptions import ClientError
 
 #Use encrypted environment variables in Lambda, fallback to awscli creds on dev machine
@@ -14,12 +11,12 @@ try:
     ENCRYPTED_SECRET_KEY = os.environ['SECRET_ACCESS_KEY']
     DECRYPTED_SECRET_KEY = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_SECRET_KEY))['Plaintext'].decode("utf-8")
 
-    ec2 = boto3.resource('ec2', aws_access_key_id=DECRYPTED_ACCESS_KEY, aws_secret_access_key=DECRYPTED_SECRET_KEY)
+    EC2 = boto3.resource('ec2', aws_access_key_id=DECRYPTED_ACCESS_KEY, aws_secret_access_key=DECRYPTED_SECRET_KEY)
 except ClientError as ex:
     if ex.response['Error']['Code'] == 'InvalidCiphertextException':
-        ec2 = boto3.resource('ec2')
+        EC2 = boto3.resource('ec2')
 except KeyError:
-    ec2 = boto3.resource('ec2')
+    EC2 = boto3.resource('ec2')
 
 def generate_response(response_data):
     output = {
@@ -29,22 +26,22 @@ def generate_response(response_data):
                 "contentType": "PlainText",
                 "content": None
             },
-           "intentName": None,
-           "slots": None 
+            "intentName": None,
+            "slots": None
         }
     }
-    
+
     for field in response_data.keys():
         if field in ("type", "intentName", "slots"):
             output["dialogAction"][field] = response_data[field]
         elif field == "content":
             output["dialogAction"]["message"]["content"] = response_data[field]
-    print (output)
+    print(output)
     return output
 
 def get_num_instances():
     running_filter = {'Name': 'instance-state-name', 'Values': ['running']}
-    instances = ec2.instances.filter(Filters=[running_filter])
+    instances = EC2.instances.filter(Filters=[running_filter])
     running_instances = [instance.id for instance in instances]
     response_data = {
         "type": "Close",
@@ -55,18 +52,19 @@ def get_num_instances():
 
 def get_instance_status():
     status = []
-    instances = ec2.instances.filter(InstanceIds=[])
+    instances = EC2.instances.filter(InstanceIds=[])
     for instance in instances:
         if instance.platform:
             platform = instance.platform
         else:
             platform = 'Linux'
         status.append("{0}, a {1} instance, is currently {2}.".format(instance.id, platform, instance.state["Name"]))
+
     response_data = {
         "type": "Close",
         "fulfillmentState": "Fulfilled",
         "content": ''.join(status)
-    }    
+    }
     return generate_response(response_data)
 
 def lambda_handler(event, context):
@@ -75,9 +73,8 @@ def lambda_handler(event, context):
         output = get_num_instances()
     elif event["currentIntent"]["name"] == "InstanceStatus":
         output = get_instance_status()
-        
+
     if output is not None:
         return output
     else:
         return None
-
--- tests.py
+++ tests.py
@@ -1,49 +1,46 @@
 import unittest
-import lambda_function
 import re
+import lambda_function
 
 class TestReadFunctions(unittest.TestCase):
-
     def setUp(self):
         self.mock_event = {
-          "currentIntent": {
-            "name": "intent-name",
-            "slots": {
-              "slot-name": "value",
-              "slot-name": "value",
-              "slot-name": "value"
+            "currentIntent": {
+                "name": "intent-name",
+                "slots": {
+                    "slot-name": "value"
+                },
+                "confirmationStatus": "None, Confirmed, or Denied",
             },
-            "confirmationStatus": "None, Confirmed, or Denied (intent confirmation, if configured)",
-          },
-          "bot": {
-            "name": "bot-name",
-            "alias": "bot-alias",
-            "version": "bot-version"
-          },
-          "userId": "user-id specified in the POST request to Amazon Lex.",
-          "inputTranscript": "Text used to process the request",
-          "invocationSource": "FulfillmentCodeHook or DialogCodeHook",
-          "outputDialogMode": "Text or Voice, based on ContentType request header in runtime API request",
-          "messageVersion": "1.0",
-          "sessionAttributes": { 
-             "key1": "value1",
-             "key2": "value2"
-          }
+            "bot": {
+                "name": "bot-name",
+                "alias": "bot-alias",
+                "version": "bot-version"
+            },
+            "userId": "user-id specified in the POST request to Amazon Lex.",
+            "inputTranscript": "Text used to process the request",
+            "invocationSource": "FulfillmentCodeHook or DialogCodeHook",
+            "outputDialogMode": "Text or Voice",
+            "messageVersion": "1.0",
+            "sessionAttributes": {
+                "key1": "value1",
+                "key2": "value2"
+            }
         }
 
     def test_num_instances(self):
         self.mock_event["currentIntent"]["name"] = "RunningInstances"
         output = lambda_function.lambda_handler(self.mock_event, None)
-        regex = re.compile('There are [1-9]\d* instances running\.')
+        regex = re.compile(r'There are [1-9]\d* instances running\.')
         match = regex.match(output["dialogAction"]["message"]["content"])
         self.assertIsNotNone(match)
 
     def test_instance_status(self):
         self.mock_event["currentIntent"]["name"] = "InstanceStatus"
         output = lambda_function.lambda_handler(self.mock_event, None)
-        regex = re.compile('(i-[a-f0-9]{8,}, a (Windows|Linux) instance, is currently (pending|running|shutting down|terminated|stopping|stopped)\.)+')
+        regex = re.compile(r'(i-[a-f0-9]{8,}, a (Windows|Linux) instance, is currently (pending|running|shutting down|terminated|stopping|stopped)\.)+')
         match = regex.match(output["dialogAction"]["message"]["content"])
         self.assertIsNotNone(match)
-        
+
 if __name__ == '__main__':
     unittest.main()
